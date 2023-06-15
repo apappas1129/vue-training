81bac687c60e61b1ede61ec876fb5fcddce8dc4e
Alex Pappas <a.pappas@arcanys.com>
2023-06-15 10:55:35 +0800

    🏗 added vite-plugin-ssr in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ssr from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ssr(),
   ],
 })
