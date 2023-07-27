3c005349421b8595dfae96105995998694ffc7f5
Alex Pappas <a.pappas@arcanys.com>
2023-07-28 00:40:46 +0800

    🔥 notes

--- server/middlewares/ssr.middleware.ts
+++ server/middlewares/ssr.middleware.ts
@@ -3,12 +3,6 @@ import { renderPage } from 'vite-plugin-ssr/server';
 import { PageContextServer } from '#root/renderer/types';
 
 const ssr: RequestHandler = async (req, res, next) => {
-  /** NOTE: Although the actual login and session cookie is done on the Web API, and the domain set on
-   * the development express-session config is `localhost`, the address only treated as same-origin
-   * by the browser seems to be specific with the port of the web application server. Kind of confusing
-   * but this is what I have observed ATTOW. The session cookie is not automatically attached by the browser
-   * on the requests to the Web API. Hence, manually passing the cookie on the pageContext below and manually
-   * inserting the headers on my API requests on the Vue side. */
   console.log('\x1b[32m%s\x1b[0m', '※ SSR Entrypoint Express-session middleware');
   console.log('\t[Request Headers Cookie]\n\t', req.headers.cookie);
   console.log('\t[REDIS SESSION USER]\n\t', req.session?.user);
@@ -19,6 +13,9 @@ const ssr: RequestHandler = async (req, res, next) => {
     ability: req.ability,
   };
 
+  // This is no longer needed if only for manually attaching cookie headers to API requests.
+  // Keeping below for future reference that this can be done as well. But adding `credentials = 'include'`
+  // to the ofetch FetchOptions automatically carries over the session cookies for the same-origin domain of the request
   if (req.headers?.cookie) pageContextInit.headers = { cookie: req.headers.cookie };
 
   const pageContext = await renderPage(pageContextInit);
